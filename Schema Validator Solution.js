a9a539dad180a3d36e29e6af0c18f12f89e91101
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-07-22 15:39:03 -0400

    Update comment header

--- Schema Validator Solution.js
+++ Schema Validator Solution.js
@@ -1,13 +1,11 @@
 /**
+Date: 06-01-2026
+Instructions:
 Given an object (JavaScript) or dictionary (Python), determine if it matches the following schema:
-
 "{username: string}"
-
 Extra keys are allowed
 */
 
 function isValidSchema(obj) {
-
   return (Object.hasOwn(obj, "username") && (typeof obj.username == "string"))
-
 }
